3f9db62942d7c821e41b471af0cea89d7a4e8fc0
fpineda1410 <fpineda147@outlook.com>
2021-05-28 19:50:34 -0600

    get example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ArtistsComponent } from './components/artists/artists.component';
+import { SearchComponent } from './components/search/search.component';
+import { TarjetasComponent } from './components/tarjetas/tarjetas.component';
+import { NavComponent } from './components/shared/nav/nav.component';
+import { LoadingComponent } from './components/shared/loading/loading.component';
+import { RouterModule } from '@angular/router';
+import {ROUTES }from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArtistsComponent,
+    SearchComponent,
+    TarjetasComponent,
+    NavComponent,
+    LoadingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(ROUTES, {useHash:true}),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient }from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  paises:any = [];
+
+  constructor( private http: HttpClient) { 
+    this.http.get ('https://restcountries.eu/rest/v2/lang/es')
+    .subscribe((response:any) =>{
+      console.log(response);
+      this.paises=response;
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+}
