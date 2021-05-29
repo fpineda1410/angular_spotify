9997f9bebc31ac20efd248d7c4dd29bd8da6719f
fpineda1410 <fpineda147@outlook.com>
2021-05-28 19:52:34 -0600

    fetch deleted

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,14 +8,10 @@ import {HttpClient }from '@angular/common/http';
 })
 export class HomeComponent implements OnInit {
 
-  paises:any = [];
+
 
   constructor( private http: HttpClient) { 
-    this.http.get ('https://restcountries.eu/rest/v2/lang/es')
-    .subscribe((response:any) =>{
-      console.log(response);
-      this.paises=response;
-    });
+    
   }
 
   ngOnInit(): void {
